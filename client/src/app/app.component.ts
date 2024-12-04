c24fdee11d414c2fb6c6e12901c66e18e365b932
Samuel Pokam <samuel.pokam@cgi.com>
2024-12-04 12:24:02 -0500

    End of section 11

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Product } from './shared/models/product'
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ShopComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
