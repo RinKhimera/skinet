cacf58c0e84ec21ae68fca5ed6d1bde8712eeab0
Samuel Pokam <samuel.pokam@cgi.com>
2024-12-11 13:22:18 -0500

    End of section 14

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,15 +1,18 @@
 using System;
 using Core.Entities;
 using Infrastructure.Config;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 // Définition de la classe StoreContext qui hérite de DbContext
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
     // Définition de la propriété DbSet pour les produits
     public required DbSet<Product> Products { get; set; }
+
+    public required DbSet<Address> Addresses { get; set; }
 
     // Méthode appelée lors de la création du modèle
     protected override void OnModelCreating(ModelBuilder modelBuilder)
