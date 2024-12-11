398c7c968f8821a7be635e114e4c67174992d140
Samuel Pokam <samuel.pokam@cgi.com>
2024-12-11 10:09:44 -0500

    End of section 13

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { lastValueFrom } from 'rxjs'
 import { routes } from './app.routes'
 import { errorInterceptor } from './core/interceptors/error.interceptor'
 import { loadingInterceptor } from './core/interceptors/loading.interceptor'
+import { InitService } from './core/services/init.service'
+
+const initializeApp = (InitService: InitService) => {
+  return () =>
+    lastValueFrom(InitService.init()).finally(() => {
+      const splash = document.getElementById('initial-splash')
+      if (splash) {
+        splash.remove()
+      }
+    })
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +30,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [InitService],
+    },
   ],
 }
--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,9 +1,10 @@
 import { CurrencyPipe } from '@angular/common'
-import { Component, Input } from '@angular/core'
+import { Component, inject, Input } from '@angular/core'
 import { MatButton } from '@angular/material/button'
 import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card'
 import { MatIcon } from '@angular/material/icon'
 import { RouterLink } from '@angular/router'
+import { CartService } from '../../../core/services/cart.service'
 import { Product } from '../../../shared/models/product'
 
 @Component({
@@ -22,4 +23,5 @@ import { Product } from '../../../shared/models/product'
 })
 export class ProductItemComponent {
   @Input() product?: Product
+  cartService = inject(CartService)
 }
